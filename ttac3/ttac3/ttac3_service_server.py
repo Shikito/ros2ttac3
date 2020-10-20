60123906864495fa03732fbd30f0db8423bb61e3
Shikito <shikito.aos@gmail.com>
2020-10-20 15:43:07 +0900

    add ttac3_state_publishser

--- ttac3/ttac3/ttac3_service_server.py
+++ ttac3/ttac3/ttac3_service_server.py
@@ -16,11 +16,31 @@ class TTAC3ServiceServer(Node):
             self.move_ttac3_callback
         )
 
-        self.pub_ttac3_state = 
+        self.pub_ttac3_state = self.create_publisher(
+            Int32MultiArray,
+            'ttac3_state',
+            10
+        )
+        timer_period = 0.01 # seconds
+        self.timer = self.create_timer(timer_period, self.pub_ttac3_state_callback)
+
+        self._invisible_state = [-1, -1, -1]
+        self.ttac3_state = self._invisible_state
+    
+    def pub_ttac3_state_callback(self):
+        msg = Int32MultiArray()
+        msg.data = self.ttac3_state
+        self.pub_ttac3_state.publish(msg)
 
     def move_ttac3_callback(self, request, response):
         self.get_logger().info('Receive a request!')
+        self.ttac3_state = self._invisible_state
         self.get_logger().info(send_msg(f"xc.move_to_x_y_z({request.xyz_goal[0]},{request.xyz_goal[1]},{request.xyz_goal[2]})".encode()))
+        self.ttac3_state = [
+            request.xyz_goal[0],
+            request.xyz_goal[1],
+            request.xyz_goal[2]
+        ]
         self.get_logger().info('Done!')
 
         response.is_success = True
